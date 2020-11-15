3f3c9d786859ab3b11ba1cfbbb15dd34d7b2cf01
aru99 <mohammadarmanmavric@gmail.com>
2020-11-15 13:42:38 +0530

    added a rough outlook of the card component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
+ReactDOM.render(
+  <>
+    <div className="cards">
+      <div className="card">
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BMjhiMzgxZTctNDc1Ni00OTIxLTlhMTYtZTA3ZWFkODRkNmE2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+          alt="random photograph"
+          className="card_img"
+        />
+        <div className="card_info">
+          <span className="card_category"> A netflix original series</span>
+          <h3 className="card_title"> The Breaking Bad</h3>
+          <a
+            href="https://www.netflix.com/in/title/70143836?source=35"
+            target="_blank"
+          >
+            <button>Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </>,
+
+  document.getElementById("root")
+);
